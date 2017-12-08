d4f02a3d65ab1b057311113c6aa0f1946ebaa575
benreckas <benreckas@gmail.com>
2017-12-07 20:02:05 -0800

    Commented out server-app.js.

--- angular/server-app.js
+++ angular/server-app.js
@@ -1,9 +1,9 @@
-import { each } from '../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async';
+// import { each } from '../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async';
 //same as Traversy's "app.js" file
 
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+// var express = require('express');
+// var path = require('path');
+// var bodyParser = require('body-parser');
 
 
 
@@ -17,19 +17,19 @@ var bodyParser = require('body-parser');
 
 //var port = 3000
 
-var app = express();
+// var app = express();
 
 //View Engine
-app.set('views', path.join (__dirname, 'views'));
-app.set('view engine,' 'ejs');
-app.engine('html', require('ejs').renderFile);
+// app.set('views', path.join (__dirname, 'views'));
+// app.set('view engine,' 'ejs');
+// app.engine('html', require('ejs').renderFile);
 
 //Set Static Folder
-app.use(express.static(path.join(__dirname, 'client'));
+// app.use(express.static(path.join(__dirname, 'client'));
 
 //Body Parser MW
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 
 
@@ -40,5 +40,3 @@ app.use(bodyParser.urlencoded({extended: false}));
 //app.listen(port, function(){
     //console.log('Server started on port' +port);
 //});
-
-
