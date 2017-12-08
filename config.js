f2add2f10d994347b6058f4db732de45696c59d9
Raed <rne23@cornell.edu>
2017-12-07 19:42:52 -0800

    config

---
+++ config.js
@@ -0,0 +1,9 @@
+// Default Application Configuration Template
+
+var config = {};
+
+config.mongoUrl = "mongodb://noinstructor17:irisedapp17@ds111496.mlab.com:11496/iris-app";
+
+config.port = 3000;
+
+module.exports = config;
