dd6f1b2ccc9093422941886812a8a17fb05e9437
David Ramirez <dramirez2096@gmail.com>
2017-12-10 15:10:49 -0800

    quiz added

---
+++ angular/src/app/components/student-work/quiz/script.js
@@ -0,0 +1,128 @@
+$( init );
+
+const firstContainer = document.querySelector('#one');
+const secondContainer = document.querySelector('#two');
+
+let amount1 = 0;
+let amount2 = 0;
+
+firstContainer.innerHTML = `Angelina: ${amount1}`
+secondContainer.innerHTML = `Lacey: ${amount2}`
+
+function init() {
+ $('.apple').draggable({
+    cursor: 'move',
+    containment: 'window',
+ });
+ $('#one').droppable( {
+    drop: showAmountOne
+  });
+  $('#two').droppable( {
+    drop: showAmountTwo
+  });
+}
+
+function dropEvent( event, ui ) {
+    var draggable = ui.draggable;
+    console.log('dropped');
+  }
+
+function showAmountOne (event, ui){
+    amount1++;
+    firstContainer.innerHTML = `Angelina: ${amount1}`
+
+}
+function showAmountTwo (event, ui){
+    amount2++;
+    secondContainer.innerHTML = `Lacey: ${amount2}`
+
+}
+
+
+
+//-------------------------- Quiz Scripts ------------------------------
+const quizContainer = document.querySelector('#quiz');
+const resultsContainer = document.querySelector('#results');
+const submitButton = document.querySelector('#submit');
+
+const output = [];
+
+const quizQuestions =[
+    {
+        question: "Angelina has 12 apples and Lacey has 7. Lacey gives 2 apples to Angelina. How many more apples does Angelina have than Lacey?",
+        answers: {
+        a: "9",
+        b: "14",
+        c: "5"
+        },
+        correctAnswer: "a",
+    }
+];
+
+
+function buildQuiz(){
+    const output = [];
+
+    quizQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers =[];
+
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}"></input>
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                ); 
+            }
+            output.push(
+                `<div class="question">${currentQuestion.question}</div>
+                <div class="answers">${answers.join('')}</div>`
+            );
+        }
+    );
+    quizContainer.innerHTML = output.join(''); 
+}
+
+quizQuestions.forEach( (currentQuestion, questionNumber) => {
+    const answers = [];
+    for(letter in currentQuestion.answers){
+        answers.push(
+            `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}"></input>
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+            </label>` 
+        );
+    }
+    output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+    );
+    quizContainer.innerHTML = output.join('');
+  });
+
+function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    quizQuestions.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if(userAnswer===currentQuestion.correctAnswer){
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+            answerContainers[questionNumber].style.color = 'red';
+          }
+          resultsContainer.innerHTML = `You got ${numCorrect} out of ${quizQuestions.length} questions correct.`;
+    });
+}
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
