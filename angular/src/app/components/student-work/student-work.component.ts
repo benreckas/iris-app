9a81e0d36b6d0ec6f30e802873fd312a1315fd9b
David Ramirez <dramirez2096@gmail.com>
2017-12-20 17:16:28 -0800

    game scripts working

--- angular/src/app/components/student-work/student-work.component.ts
+++ angular/src/app/components/student-work/student-work.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-student-work',
   templateUrl: './student-work.component.html',
@@ -11,6 +13,50 @@ export class StudentWorkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('JavaScript goes here!');
+
+$( init );
+
+const firstContainer = document.querySelector('#one');
+const secondContainer = document.querySelector('#two');
+
+let amount1 = 0;
+let amount2 = 0;
+
+firstContainer.innerHTML = `Angelina: ${amount1}`
+secondContainer.innerHTML = `Lacey: ${amount2}`
+
+function init() {
+ $('.apple').draggable({
+    cursor: 'move',
+    containment: 'window',
+ });
+ $('#one').droppable( {
+    drop: addAmountOne,
+    out: subAmountOne
+  });
+  $('#two').droppable( {
+    drop: addAmountTwo,
+    out: subAmountTwo
+  });
+}
+
+function addAmountOne (event, ui){
+  amount1++;
+  firstContainer.innerHTML = `Angelina: ${amount1}`
+}
+function subAmountOne (event, ui){
+  amount1--;
+  firstContainer.innerHTML = `Angelina: ${amount1}`
+}
+function addAmountTwo (event, ui){
+  amount2++;
+  secondContainer.innerHTML = `Lacey: ${amount2}`
+}
+function subAmountTwo (event, ui){
+  amount2--;
+  secondContainer.innerHTML = `Lacey: ${amount2}`
+}
   }
 
 }
