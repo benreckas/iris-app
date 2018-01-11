bd45ced331300b828265df2ca79957742dd9fd78
David Cervantes <pakawaka1@gmail.com>
2018-01-11 07:49:07 -0800

    port settings changed to 5000

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const passport = require('passport');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 
 //Passport Middleware
--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = config;
 
 config.mongoUrl = "mongodb://noinstructor17:irisedapp17@ds111496.mlab.com:11496/iris-app";
 
-config.port = 3000;
+config.port = 5000;
 
 config.secret = "your secret";
